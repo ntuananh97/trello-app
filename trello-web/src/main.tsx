7a3e2f5276f96d1c0025b4fa12593fb353bda40a
tuananh <anh.nguyentuan@euroland.com>
2024-06-04 14:38:29 +0700

    fix: mui style is override the custom style

--- trello-web/src/main.tsx
+++ trello-web/src/main.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 import { CssBaseline } from '@mui/material'
+import StyledEngineProvider from "@mui/material/StyledEngineProvider";
 import { AppProvider } from './AppContext.tsx';
 import './styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppProvider>
-      <CssBaseline />
-      <App />
+      <StyledEngineProvider injectFirst>
+        <CssBaseline />
+        <App />
+      </StyledEngineProvider>
     </AppProvider>
   </React.StrictMode>
 );
