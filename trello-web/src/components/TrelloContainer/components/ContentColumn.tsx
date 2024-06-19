98b207e4209488cae1d1b497598a8b80372707f1
LAPTOP-UMM7HUUJ\user <ntuananh2897@gmail.com>
2024-06-19 07:31:25 +0700

    feat: integrate dndkit into app

--- trello-web/src/components/TrelloContainer/components/ContentColumn.tsx
+++ trello-web/src/components/TrelloContainer/components/ContentColumn.tsx
@@ -4,8 +4,8 @@ import ContentColumnList from "../../ContentColumnList";
 import TrelloContentFooter from "./TrelloContentFooter";
 import ContentColumnHeader from "./ContentColumnHeader";
 import { useGetColumnDetailById } from '../../../hooks/useTrelloContent';
-
-
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 interface ContentColumnProps {
   columnId: string
@@ -14,9 +14,29 @@ interface ContentColumnProps {
 const ContentColumn: React.FC<ContentColumnProps> = ({columnId}) => {
   const columnData = useGetColumnDetailById(columnId);
 
-  console.log("columnId:", columnData)
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+  } = useSortable({id: columnId});
+
+  const dndKitColumnStyles = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
   return (
-    <Box className="trello-column__item" component="li" sx={{ maxHeight: "100%" }}>
+    <Box
+      className="trello-column__item"
+      component="li"
+      sx={{ maxHeight: "100%" }}
+      ref={setNodeRef}
+      style={dndKitColumnStyles}
+      {...attributes}
+      {...listeners}
+    >
       <Paper
         elevation={3}
         sx={{
@@ -25,7 +45,7 @@ const ContentColumn: React.FC<ContentColumnProps> = ({columnId}) => {
         }}
         className="trello-column__item__container"
       >
-        <ContentColumnHeader title={columnData.title}  />
+        <ContentColumnHeader title={columnData.title} />
         <ContentColumnList columnId={columnId} />
         <TrelloContentFooter />
       </Paper>
--- trello-web/src/components/TrelloContainer/components/TrelloContent.tsx
+++ trello-web/src/components/TrelloContainer/components/TrelloContent.tsx
@@ -2,24 +2,63 @@ import Box from '@mui/material/Box'
 import ContentColumn from './ContentColumn'
 import AddContentColumn from './AddContentColumn';
 import { useSelector } from 'react-redux';
+import {
+  DndContext,
+  DragOverlay,
+  closestCorners,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  DragEndEvent
+} from "@dnd-kit/core";
 import { columnOrderIdSelector } from '../../../redux/trelloContent/trelloContentSelector';
+import { SortableContext, horizontalListSortingStrategy } from '@dnd-kit/sortable';
 
 
 function TrelloContent() {
   const columnOrderIds = useSelector(columnOrderIdSelector);
-  console.log("TrelloContent ~ columnOrderIds:", columnOrderIds)
+  console.log("TrelloContent ~ columnOrderIds:", columnOrderIds);
+
+  const handleDragStart = () => {
+    console.log('drag start');
+    
+  }
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    console.log('drag end', event);
+    const { active, over } = event;
+    const { id: activeId } = active
+    const overId = over?.id;
+
+    if (!overId) return;
+
+    const notSortable = activeId === overId;
+    if (notSortable) return;
+
+    
+    
+  }
 
   return (
-    <Box
-      className="trello-column__list"
-      component="ul"
-      sx={{ display: "flex", gap: "20px", height: '100%' }}
+    <DndContext
+      // collisionDetection={closestCorners}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
     >
-      {columnOrderIds.map((columnId) => (
-        <ContentColumn key={columnId} columnId={columnId} />
-      ))}
-      <AddContentColumn />
-    </Box>
+      <SortableContext items={columnOrderIds} strategy={horizontalListSortingStrategy}>
+        <Box
+          className="trello-column__list"
+          component="ul"
+          sx={{ display: "flex", gap: "20px", height: "100%" }}
+        >
+          {columnOrderIds.map((columnId) => (
+            <ContentColumn key={columnId} columnId={columnId} />
+          ))}
+          <AddContentColumn />
+        </Box>
+      </SortableContext>
+    </DndContext>
   );
 }
 
